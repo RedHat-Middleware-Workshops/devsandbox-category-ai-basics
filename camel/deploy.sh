e18b2de61dbec0a644150e328311620e1f990985
brunoNetId <bruno.meseguer@gmail.com>
2024-12-03 22:30:10 +0000

    Camel instead of Camel K

--- camel/deploy.sh
+++ camel/deploy.sh
@@ -1,3 +1,18 @@
-kamel run price.yaml \
--t knative-service.enabled=false \
---resource file:price-catalogue.json
+# Old Camel K deployment
+# kamel run price.yaml \
+# -t knative-service.enabled=false \
+# --resource file:price-catalogue.json
+
+# Setup of Camel JBang and Kubernetes plugin
+source ~/.bashrc && \
+jbang cache clear && \
+jbang app uninstall camel && \
+jbang trust add https://github.com/apache/camel/ && \
+jbang app install -Dcamel.jbang.version=4.8.0 camel@apache/camel && \
+jbang camel@apache/camel plugin add kubernetes
+
+# Deploy Camel integration
+camel kubernetes run \
+price.yaml price-catalogue.json \
+--cluster-type=openshift \
+--trait container.name=price
